6b3b83e711f6225d6d44fa1551faa02310d39737
enriqueesanchz <enrsancar1@alum.us.es>
2023-11-30 01:41:41 +0100

    style sample_data

--- data/sample_data.py
+++ data/sample_data.py
@@ -1,26 +1,26 @@
+import os
 import threading
 import time
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 from sense_hat import SenseHat
-import os
 
 data = [[],[],[],[],[],[]]
-is_running = True  # Bandera para controlar la ejecución del hilo de generación de datos
 
 def generate_data():
-    archivo="datos" # nombre del archivo
-    tipo_archivo=".csv" # extensión 
-    t=[]
-    t_sample=[]
-    t_ini=time.time()
-    t_ant=t_ini
-    sense=SenseHat()
-    muestras=100
-    for i in range(muestras):
-        t_actual=time.time()
+    ARCHIVO = "datos.csv" # nombre del archivo
+    t = []
+    t_sample = []
+    t_ini = time.time()
+    t_ant = t_ini
+    sense = SenseHat()
+    muestras = 100
+
+    for _ in range(muestras):
+        t_actual = time.time()
         acceleration = sense.get_accelerometer_raw()
+
         #datos de aceleración en Gs
         data[0].append(acceleration['x'])
         data[1].append(acceleration['y'])
@@ -30,24 +30,27 @@ def generate_data():
         data[3].append(gyroscope['x'])
         data[4].append(gyroscope['y'])
         data[5].append(gyroscope['z'])
-        
+
         t.append(t_actual-t_ini)
         t_sample.append(t_actual-t_ant)
-        
-        t_ant=t_actual
-    datosIMU=pd.DataFrame(list(zip(t,data[0],data[1],data[2],data[3],data[4],data[5])),
-                          columns =['t', 'accel_x','accel_y','accel_z','gyro_x','gyro_y','gyro_z'])
-    intentos=0
-    while(os.path.isfile(archivo+tipo_archivo)):
+
+        t_ant = t_actual
+
+    datos_imu = pd.DataFrame(list(zip(t,data[0],data[1],data[2],data[3],data[4],data[5])),
+                            columns =['t', 'accel_x','accel_y','accel_z','gyro_x','gyro_y','gyro_z'])
+
+    intentos = 0
+    while os.path.isfile(ARCHIVO):
         #En caso de que exista un fichero con dicho nombre, le pone un numero al final (consecutivo)
-        print("El archivo",archivo,"ya existe")
-        intentos+=1
-        if(intentos>1):
-            archivo=archivo[:-1]
-        archivo=archivo+str(intentos)
-    print("Se guardará el archivo con nombre: ",archivo)
+        print("El archivo", ARCHIVO, "ya existe")
+        intentos += 1
+        if intentos > 1:
+            ARCHIVO = ARCHIVO[:-1]
+        ARCHIVO = ARCHIVO+str(intentos)
+
+    print("Se guardará el archivo con nombre: ", ARCHIVO)
     # Crea un archivo csv y guarda los datos
-    datosIMU.to_csv(archivo+tipo_archivo,index=False)
+    datos_imu.to_csv(ARCHIVO, index=False)
 
 
 def update_plot(frame):
@@ -64,8 +67,8 @@ generate_thread = threading.Thread(target=generate_data)
 generate_thread.start()
 
 # Configurar la animación en el hilo principal
-animation = FuncAnimation(plt.gcf(), update_plot, interval=200,
-                            cache_frame_data=True, save_count=100)  # Intervalo en milisegundos
+animation = FuncAnimation(plt.gcf(), update_plot, interval=200, 
+                        cache_frame_data=True, save_count=100)  # Intervalo en milisegundos
 
 # Mostrar la ventana del gráfico
 plt.show()
